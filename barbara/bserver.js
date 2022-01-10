f11186a8e6e8e08f45cb8adbe75d44cb2622a38d
Barbara <barbara.branco.c@gmail.com>
2022-01-10 19:35:38 +0100

    misaupoint/daybefore

--- barbara/bserver.js
+++ barbara/bserver.js
@@ -10,5 +10,19 @@ fs.readFile("./content/bdatas.json", (error, datas) =>{
    datas = datas.toString();
    datas = JSON.parse(datas);
    
-   datas.test
+   datas.test.forEach((item_categ) => { //demander à Loïc 
+    console.log(" " + item_categ.name)
+
+   item_categ.secondtest.forEach((item_subcateg) =>{
+       console.log("   " + item_subcateg.name)
+
+       item_subcateg.thirdtest.forEach((item_subsubcateg) =>{
+           console.log("      "  + item_subsubcateg.name)
+
+           item_subsubcateg.fourthtest.forEach((item_fourthcateg) => {
+               console.log("_________" + item_fourthcateg.name)
+           })
+       })
+   })
+});
 })
